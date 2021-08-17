db7b3a9cbce905bd2efc96fb0e50e83af84c3011
Romario Negreiros <nromario482@gmail.com>
2021-08-16 22:58:27 -0300

    organized code + function to set the map view and mark

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,36 @@
+const errBox = $('.error_warning')
+errBox.hide();
+
 const api_url = 'https://geo.ipify.org/api/v1';
 const api_key = 'at_MtyFjPLjofX68L8nEinBb0M2EZiYn&ipAddress';
+
 const regexIp = /[0-9]{1,3}[.]{1}[0-9]{1,3}[.]{1}[0-9]{1,3}[.]{1}[0-9]{1,3}/;
 const regexDomain = /^[aA-zZ]*[.]{1}[aA-zZ]{2,3}$/;
+
+const map = L.map('map');
+const marker = L.marker([0, 0]).addTo(map);
+L.tileLayer(
+    'https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=pcxbDReDUNkbbfCq88KM',
+    {
+        attribution:
+            '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
+    }
+).addTo(map);
+
+// Mode === search by ip address or domain
+const getInputValueAndMode = () => {
+    const inputValue = $('#text').val();
+    if (inputValue.match(regexIp)) {
+        getIpAddressOrDomainData('ipAddress', inputValue);
+    } else if (inputValue.match(regexDomain)) {
+        getIpAddressOrDomainData('domain', inputValue);
+    } else {
+        errBox.append(
+            'Insert a valid ip address, such as 8.8.8.8, or a valid domain, like nodejs.org'
+        );
+        errBox.fadeIn().delay(5000).slideUp();
+    }
+};
 
 const getIpAddressOrDomainData = (mode, value) => {
     $('#text').val('');
@@ -12,57 +41,43 @@ const getIpAddressOrDomainData = (mode, value) => {
             const {
                 ip,
                 isp,
-                location: { city, country, postalCode, timezone },
+                location: { city, country, postalCode, timezone, lat, lng },
             } = data;
-            console.log(ip, isp, city, country, postalCode, timezone);
             $('.ip').html(ip);
             $('.isp').html(isp);
             $('.location').html(`${city}, ${country}, ${postalCode}`);
             $('.timezone').html('UTC ' + timezone);
+            setMap(lat, lng);
         })
         .catch(err => {
             const getErrMsg = err.responseJSON.messages;
-            console.log(getErrMsg);
+            errBox.append(getErrMsg);
+            errBox.fadeIn().delay(1500).slideUp();
         });
 };
 
-const getLocation = () => {
-    if (navigator.geolocation)
-        navigator.geolocation.getCurrentPosition(
-            position => {
-                const {
-                    coords: { latitude, longitude },
-                } = position;
-                const map = L.map('map').setView([latitude, longitude], 10);
-                L.tileLayer(
-                    'https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=pcxbDReDUNkbbfCq88KM',
-                    {
-                        attribution:
-                            '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
-                    }
-                ).addTo(map);
-            },
-            err => console.log(err)
-        );
-    else alert('Sorry, but your browser');
+// Set new view and maker lat and lng
+const setMap = (latitude = 1, longitude = 0) => {
+    map.setView([latitude, longitude], 10);
+    L.marker([latitude, longitude]).addTo(map);
 };
 
 // Loads LeaftletJS either in the current user's location or in a default place
 $(() => {
-    getLocation();
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            const {
+                coords: { latitude, longitude },
+            } = position;
+            setMap(latitude, longitude);
+        });
+    } else setMap();
 });
 
+// Event handlers for make the search
 $(() => {
-    $('#search').on('click', () => {
-        const inputValue = $('#text').val();
-        if (inputValue.match(regexIp)) {
-            getIpAddressOrDomainData('ipAddress', inputValue);
-        } else if (inputValue.match(regexDomain)) {
-            getIpAddressOrDomainData('domain', inputValue);
-        } else {
-            alert(
-                'Insert a valid ip adress, like: 8.8.8.8, or a valid domain, like: nodejs.org'
-            );
-        }
+    $('#search').on('click', getInputValueAndMode);
+    $('#text').on('keypress', ({ key }) => {
+        key === 'Enter' ? getInputValueAndMode() : '';
     });
 });
